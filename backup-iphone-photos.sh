21cd3f50f8132637fda16516b87f8f4d3513842f
hengtseChou <hankthedev@gmail.com>
2024-10-26 23:44:08 +0800

    add: use option to specify source and dest; add -l for showing less output

--- backup-iphone-photos.sh
+++ backup-iphone-photos.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
-set -e
-
 _indent() { sed 's/^/  /'; }
 
 _help() {
-    echo "Usage: backup-iphone-photos SOURCE DEST"
-    echo -e "Sync files from subfolders in SOURCE to DEST\nThen organize them into new yyyy-mm folders base on timestamps" | _indent
+    echo "Usage: backup-iphone-photos [-s SOURCE] [-d DEST] [-l]"
+    echo -e "Sync files from subfolders in SOURCE to DEST\nThen organize them into new yyyy-mm folders based on timestamps" | _indent
     echo "Options:"
     echo "--help  Display this help message and exit" | _indent
+    echo "-s      Specify the source directory" | _indent
+    echo "-d      Specify the destination directory" | _indent
+    echo "-l      Show less output" | _indent
 }
 
-if [ "$1" == "--help" ]; then
-     _help
-    exit 0
-fi
-if [ "$#" -ne 2 ]; then
-    echo "Error: Invalid number of arguments."
+source_dir=""
+dest_dir=""
+less_output=false
+
+while getopts ":s:d:l-:" opt; do
+    case $opt in
+    s) source_dir="$OPTARG" ;;
+    d) dest_dir="$OPTARG" ;;
+    l) less_output=true ;;
+    -)
+        case "${OPTARG}" in
+        help)
+            _help
+            exit 0
+            ;;
+        *)
+            echo "Invalid option: --${OPTARG}" >&2
+            _help
+            exit 1
+            ;;
+        esac
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        _help
+        exit 1
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        _help
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$source_dir" ] || [ -z "$dest_dir" ]; then
+    echo "Error: Source and destination directories are required."
     _help
     exit 1
 fi
-
-source_dir="$1"
-dest_dir="$2"
 if ! command -v rsync 2>&1 >/dev/null; then
     echo "Error: rsync not found. Make sure you have rsync installed on your system."
     exit 1
@@ -47,16 +76,22 @@ else
 fi
 
 echo ":: Backup started."
-sleep 0.5
+folders=("$source_dir"/*/)
+first=$(basename "${folders[0]}")
+last=$(basename "${folders[-1]}")
+echo ":: Syncing from $first to $last."
+sleep 1
 
 # Iterate through each subfolder in the source directory
-for source_subfolder in "$source_dir"/*/; do
+for source_subfolder in "${folders[@]}"; do
 
     subfolder_name=$(basename "$source_subfolder")
     # Check if the current subfolder has been processed before
     # The last synced subfolder will be re-checked again
     if [[ "$processed_subfolders" =~ $subfolder_name && "$subfolder_name" != "${processed_subfolders##* }" ]]; then
-        echo ":: Skipping fully processed subfolder: $subfolder_name"
+        if [ "$less_output" = false ]; then
+            echo ":: Skipping fully processed subfolder: $subfolder_name"
+        fi
         continue
     fi
 
@@ -79,11 +114,11 @@ for source_subfolder in "$source_dir"/*/; do
     if [ -z "$processed_subfolders" ]; then
         processed_subfolders="$subfolder_name"
     else
-    	processed_subfolders="$processed_subfolders $subfolder_name"
+        processed_subfolders="$processed_subfolders $subfolder_name"
     fi
-    echo "$processed_subfolders" > "$record"
+    echo "$processed_subfolders" >"$record"
 done
 
 echo ":: Backup completed."
-echo ":: Backup folder usage: $(du -sh $source_dir | cut -f1)."
-
+echo ":: Calculating backup storage usage..."
+echo ":: Backup stroage usage: $(du -sh $source_dir | cut -f1)."
