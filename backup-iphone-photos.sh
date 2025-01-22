0d852b846f146517b015d522f4a19f778a505c0c
hengtseChou <hankthedev@gmail.com>
2025-01-22 20:14:39 +0800

    fix option parsing and modify message output

--- backup-iphone-photos.sh
+++ backup-iphone-photos.sh
@@ -27,40 +27,38 @@ while getopts ":s:d:l-:" opt; do
       exit 0
       ;;
     *)
-      echo "Invalid option: --${OPTARG}" >&2
+      echo "[ERROR] Invalid option: --${OPTARG}" >&2
       _help
       exit 1
       ;;
     esac
     ;;
   \?)
-    echo "Invalid option: -$OPTARG" >&2
-    _help
-    exit 1
-    ;;
-  :)
-    echo "Option -$OPTARG requires an argument" >&2
+    echo "[ERROR] Invalid option: -$OPTARG" >&2
     _help
     exit 1
     ;;
   esac
 done
 
-if [ -z "$source_dir" ] || [ -z "$dest_dir" ]; then
-  echo "Error: Source and destination directories are required"
+if [ $OPTIND -eq 1 ]; then
   _help
   exit 1
 fi
+if [ -z "$source_dir" ] || [ -z "$dest_dir" ]; then
+  echo "[ERROR] Specify source and destination directories by -s and -d options"
+  exit 1
+fi
 if ! command -v rsync 2>&1 >/dev/null; then
-  echo "Error: rsync not found. Make sure you have rsync installed on your system"
+  echo "[ERROR] rsync not found. Make sure you have rsync installed on your system"
   exit 1
 fi
 if [ ! -d "$source_dir" ]; then
-  echo "Error: Source directory '$source_dir' does not exist"
+  echo "[ERROR] Source directory '$source_dir' does not exist"
   exit 1
 fi
 if [ ! -d "$dest_dir" ]; then
-  echo "Error: Destination directory '$dest_dir' does not exist"
+  echo "[ERROR] Destination directory '$dest_dir' does not exist"
   exit 1
 fi
 
@@ -75,11 +73,11 @@ else
   processed_subfolders=""
 fi
 
-echo ":: Backup started."
+echo "[INFO] Backup started"
 folders=("$source_dir"/*/)
 first=$(basename "${folders[0]}")
 last=$(basename "${folders[-1]}")
-echo ":: Syncing from $first to $last"
+echo "[INFO] Syncing from $first to $last"
 sleep 1
 
 # Iterate through each subfolder in the source directory
@@ -90,7 +88,7 @@ for source_subfolder in "${folders[@]}"; do
   # The last synced subfolder will be re-checked again
   if [[ "$processed_subfolders" =~ $subfolder_name && "$subfolder_name" != "${processed_subfolders##* }" ]]; then
     if [ "$less_output" = false ]; then
-      echo ":: Skipping fully processed subfolder: $subfolder_name"
+      echo "--> Skipping fully processed subfolder: $subfolder_name"
     fi
     continue
   fi
@@ -104,11 +102,11 @@ for source_subfolder in "${folders[@]}"; do
       last_modified=$(stat -c %Y "$file")
       year_month=$(date -d "@$last_modified" +%Y-%m)
       mkdir -p "$dest_dir/$year_month"
-      echo -ne "\\r:: Processing $subfolder_name: file $((i + 1)) of $total_files"
+      echo -ne "\\r--> Processing $subfolder_name: file $((i + 1)) of $total_files"
       rsync -a --ignore-existing "$file" "$dest_dir/$year_month/"
     fi
   done
-  echo -e "\\n:: $subfolder_name completed"
+  echo -e "\\n[INFO] $subfolder_name completed"
 
   # Update the processed record
   if [ -z "$processed_subfolders" ]; then
@@ -119,6 +117,6 @@ for source_subfolder in "${folders[@]}"; do
   echo "$processed_subfolders" >"$record"
 done
 
-echo ":: Backup completed"
-echo ":: Calculating backup storage usage..."
-echo ":: Backup stroage usage: $(du -sh $source_dir | cut -f1)"
+echo "[INFO] Backup completed"
+echo "[INFO] Calculating backup storage usage..."
+echo "[INFO] Backup stroage usage: $(du -sh $source_dir | cut -f1)"
